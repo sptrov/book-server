4ba4e2d9e11b28c826b2265e344ba9abaf59a911
Stoyan Petrov <stoyanpetrov@mail.bg>
2025-03-19 19:04:02 +0200

    few tests

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -22,6 +22,16 @@ describe("Server", () => {
     expect(response.body.encryptedKey).toBeDefined();
     expect(response.body.encryptedData).toBeDefined();
   });
+  it("should return error on validation error", async () => {
+    const publicClientKey = Keys.getPublicKeyBase64();
+    const response = await request(app)
+      .get("/books")
+      .set("x-public-client-key", publicClientKey)
+      .query({ query: "te" });
+    expect(response.status).toBe(400);
+    expect(response.body).toBeDefined();
+    expect(response.body.error).toBeDefined();
+  });
 
   it("should create a new book", async () => {
     const book = {
@@ -37,5 +47,23 @@ describe("Server", () => {
       .send({ encrypted: encryptedData });
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Book saved");
+  });
+
+  it("try create a new book but validation is not passed", async () => {
+    const book = {
+      title: "T",
+      author: "A",
+      publicationDate: new Date().toISOString(),
+    };
+    const publicClientKey = Keys.getPublicKeyBase64();
+    const encryptedData = Keys.encryptData(book, publicClientKey);
+
+    const response = await request(app)
+      .post("/books")
+      .send({ encrypted: encryptedData });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(
+      "Title is required, Author name is required or too short"
+    );
   });
 });
